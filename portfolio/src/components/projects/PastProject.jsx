077e7deb8a99a849c7d3d51d45de95698e3faee9
johnmartinroque <johnmartinroque@gmail.com>
2025-08-24 11:38:15 +0800

    vercel update check

--- portfolio/src/components/projects/PastProject.jsx
+++ portfolio/src/components/projects/PastProject.jsx
@@ -14,7 +14,7 @@ function PastProject() {
             <div className="flex flex-col items-center pb-10">
               <img
                 className="w-24 h-24 mb-3 rounded-full shadow-lg"
-                src="/images/TaskHive.png"
+                src="/images/Shareable.png"
                 alt="TaskHive"
               />
               <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
@@ -25,12 +25,12 @@ function PastProject() {
               </span>
               <div className="flex mt-4 md:mt-6">
                 <a
-                  href="https://taskhive-8b031.web.app"
+                  href="https://github.com/Ikawari-s/Shareable"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  Demo
+                  View Repository
                 </a>
               </div>
             </div>
@@ -45,19 +45,19 @@ function PastProject() {
                 alt="TaskHive"
               />
               <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-                TaskHive
+                LeRecipe
               </h5>
               <span className="text-sm text-gray-500 dark:text-gray-400">
                 Visual Designer
               </span>
               <div className="flex mt-4 md:mt-6">
                 <a
-                  href="https://taskhive-8b031.web.app"
+                  href="https://github.com/johnmartinroque/LeRecipe"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  Demo
+                  View Repository
                 </a>
               </div>
             </div>
@@ -84,7 +84,7 @@ function PastProject() {
                   rel="noopener noreferrer"
                   className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  Demo
+                  View Repository
                 </a>
               </div>
             </div>
