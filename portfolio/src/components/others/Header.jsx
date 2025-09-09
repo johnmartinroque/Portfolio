41c3c1d197f9b91d254f9cc6d6ec763969ba93a6
johnmartinroque <johnmartinroque@gmail.com>
2025-09-09 18:06:06 +0800

    header removed dark/light mode and added links

--- portfolio/src/components/others/Header.jsx
+++ portfolio/src/components/others/Header.jsx
@@ -78,6 +78,33 @@ function Header() {
                   Home
                 </Link>
               </li>
+              <li>
+                <Link
+                  to="/"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                  aria-current="page"
+                >
+                  About Me
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                  aria-current="page"
+                >
+                  Projects
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/"
+                  className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent"
+                  aria-current="page"
+                >
+                  Skills
+                </Link>
+              </li>
               {/*          <li>
                 <Link
                   to="/contact"
