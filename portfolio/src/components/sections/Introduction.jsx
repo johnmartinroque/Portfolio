76439d6d82177484648faf2a80d49a5f99e1482e
johnmartinroque <johnmartinroque@gmail.com>
2025-09-19 20:56:48 +0800

    view cv button

--- portfolio/src/components/sections/Introduction.jsx
+++ portfolio/src/components/sections/Introduction.jsx
@@ -31,15 +31,14 @@ function Introduction() {
             through code.
           </p>
 
-          {/*  
           <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
             <a
-              href="/file/resume.pdf"
+              href="/images/CurriculumVitae.jpg"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300"
             >
-              Hire me pls
+              View My CV
               <svg
                 className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
                 aria-hidden="true"
@@ -50,20 +49,21 @@ function Introduction() {
                 <path
                   stroke="currentColor"
                   strokeLinecap="round"
-                  strokeLinejoin="round"  
+                  strokeLinejoin="round"
                   strokeWidth="2"
                   d="M1 5h12m0 0L9 1m4 4L9 9"
                 />
               </svg>
             </a>
+            {/*
             <a
               href="#about"
               className="inline-flex justify-center items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:text-gray-900 hover:bg-gray-100 focus:ring-4 focus:ring-gray-400"
             >
               Learn more
             </a>
-          </div>
      */}
+          </div>
         </div>
       </section>
     </div>
